9d148d6f1f017107aff65dbc6ef22528f0ebd1e8
pdfosborne <pdfosborne@gmail.com>
2025-04-14 13:35:30 +0100

    Engine restored to basic setup

--- environment/engine.py
+++ environment/engine.py
@@ -42,67 +42,33 @@ class Engine:
         # Initialize history
         self.action_history = []
         self.obs_history = []
+        self.action_limit = local_setup_info['action_limit']
 
     def reset(self, start_obs:any=None):
         """Fully reset the environment."""
         obs, _ = self.Environment.reset()
         self.action_history = []
         self.obs_history = []
-        return (obs,(0,0))
+        return obs
     
     def step(self, state:any=None, action:any=None):
         """Enact an action."""
         # Record action history
         self.action_history.append(action)
-        # Action space will always be numeric, step function may need to #
-        # convert to form to match underlying engine, example:
-        # self._action_to_outcome = {
-        #     0: np.array([1, 0]),
-        #     1: np.array([0, 1]),
-        #     2: np.array([-1, 0]),
-        #     3: np.array([0, -1])
-        # }
+       
         # Return outcome of action
         obs, reward, terminated, truncated, info = self.Environment.step(action)
         self.obs_history.append(obs)
-        if terminated:
-            if len(self.sub_goal_obs)>0:
-                all_found = True
-                for sub_goal in self.sub_goal_obs:
-                    if sub_goal not in self.obs_history:
-                        all_found = False
-                if all_found:
-                    reward = 1
-                else:
-                    reward = 0
-
-            if obs!=self.terminal_goal:
-                reward = -1
-            else:
-                reward = reward
-        else:
-            reward = -0.0025
-
-        if len(self.action_history)>=100:
+        
+        if len(self.action_history)>=self.action_limit:
             reward = 0
             terminated = True
-
-        # Add inventory to observation
-        if 3 in self.obs_history:
-            if 15 in self.obs_history:
-                obs_inv = (obs, (1,1))
-            else:
-                obs_inv = (obs, (1,0))
-        elif 15 in self.obs_history:
-            obs_inv = (obs, (0,1))
-        else:
-            obs_inv = (obs, (0,0))
             
-        return obs_inv, reward, terminated, info
+        return obs, reward, terminated, info
 
     def legal_move_generator(self, obs:any=None):
         """Define legal moves at each position"""
-        legal_moves = [0,1,2,3]#self.Environment.legal_moves(obs)
+        legal_moves = [0,1,2,3] #self.Environment.legal_moves(obs)
         return legal_moves
     
     def render(self, state:any=None):
