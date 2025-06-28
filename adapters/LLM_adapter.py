d465d4529181478d8d043c4c49937abc73d705ac
pdfosborne <pdfosborne@gmail.com>
2025-06-28 11:04:44 +0100

    Adapters - LLM Adapter fix input prompt

--- adapters/LLM_adapter.py
+++ adapters/LLM_adapter.py
@@ -39,7 +39,7 @@ class Adapter:
 
         self.LLM_adapter = OllamaAdapter(
             model_name=setup_info.get('model_name', 'llama3.2'),
-            base_prompt=setup_info.get('system_prompt', 'You are playing a game of Chess.'),
+            base_prompt=setup_info.get('system_prompt', 'You are playing a navigating a grid based Gym environment.'),
             context_length=2000,
             action_history_length=setup_info.get('action_history_length', 5),
             encoder=setup_info.get('encoder', 'MiniLM_L6v2')
@@ -49,7 +49,7 @@ class Adapter:
     def adapter(self, state: str, legal_moves:list = None, episode_action_history:list = None, encode:bool=True, indexed: bool = False) -> Tensor:     
         """ Use Language description for every student for current grid position """
 
-        state = self.obs_mapping[state]
+        state = 'Original environment state '+ str(state) + ', transformed language description: ' + self.obs_mapping[state]
 
 
         # Use the elsciRL LLM adapter to transform and encode
